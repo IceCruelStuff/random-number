e8be35d91bf7cdf6d25ea0f3b4c9c361a837cc5f
IceCruelStuff <minecraftcool991@gmail.com>
2020-01-09 16:54:38 -0800

    add main file

---
+++ random.php
@@ -0,0 +1,10 @@
+<?php 
+
+$num1 = rand(); 
+  
+echo "Random number: " . $num1 . "\n"; 
+  
+$num2 = rand(7, 100);  // set the numbers 7 and 100 to anything you want
+  
+  
+echo "Random number: ", $num2;
